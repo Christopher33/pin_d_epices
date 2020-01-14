aa448269fd82d5bc3c7c607af10ce4d357b22b4c
Christopher33 <christopher.couty@lapiscine.pro>
2020-01-14 17:27:54 +0100

    mise a jour du 14/01 version 1 OK

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,7 +5,13 @@ namespace App\Controller;
 
 
 use App\Entity\Commande;
+use App\Entity\Dessert;
+use App\Entity\Plat;
+use App\Form\DessertType;
+use App\Form\PlatType;
 use App\Repository\CommandeRepository;
+use App\Repository\DessertRepository;
+use App\Repository\PlatRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -56,5 +62,65 @@ class AdminController extends AbstractController
         }
 
         return $this->redirectToRoute('commande_index');
+    }
+//Controller admin for changing "Plat" entity
+    /**
+     * @Route("/plat", name="plat_index", methods={"GET"})
+     */
+    public function indexPlat(PlatRepository $platRepository): Response
+    {
+        return $this->render('plat/index.html.twig', [
+            'plats' => $platRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/plat_edit", name="plat_edit", methods={"GET","POST"})
+     */
+    public function editPlat(Request $request, Plat $plat): Response
+    {
+        $form = $this->createForm(PlatType::class, $plat);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('plat_index');
+        }
+
+        return $this->render('plat/edit.html.twig', [
+            'plat' => $plat,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/dessert", name="dessert_index", methods={"GET"})
+     */
+    public function indexDessert(DessertRepository $dessertRepository): Response
+    {
+        return $this->render('dessert/index.html.twig', [
+            'desserts' => $dessertRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/dessert_edit", name="dessert_edit", methods={"GET","POST"})
+     */
+    public function editDessert(Request $request, Dessert $dessert): Response
+    {
+        $form = $this->createForm(DessertType::class, $dessert);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('dessert_index');
+        }
+
+        return $this->render('dessert/edit.html.twig', [
+            'dessert' => $dessert,
+            'form' => $form->createView(),
+        ]);
     }
 }
---
+++ src/Form/DessertType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Dessert;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DessertType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('price')
+            ->add('poids')
+            ->add('day')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Dessert::class,
+        ]);
+    }
+}
